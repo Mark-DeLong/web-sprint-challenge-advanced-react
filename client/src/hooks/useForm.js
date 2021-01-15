c99ae2686a2af3ee31458d57d6d6a72c93bf3bf7
Mark <mark@Marks-MacBook-Air.local>
2021-01-15 14:48:40 -0500

    .

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-
+//
 export default function useForm(initialValue) {
+  //Hook
   const [showSuccessMessage, setShowSuccessMessage] = useState(false)
   const [values, setValues] = useState(initialValue)
-
+  //Change handler
   const handleChanges = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value })
   }
-
+  //Submit handler
   const handleSubmit = (e) => {
     e.preventDefault()
     setShowSuccessMessage(true)
